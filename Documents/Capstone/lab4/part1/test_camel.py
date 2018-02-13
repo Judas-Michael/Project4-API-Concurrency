1a89dd165ef772439dfe35b732e3afebc76483a1
juliechiu <jhchiu1@gmail.com>
2018-02-12 20:13:33 -0600

    update typo

---
+++ Documents/Capstone/lab4/part1/test_camel.py
@@ -0,0 +1,16 @@
+import unittest
+from camel_case import camel
+
+class TestCamelCase(unittest.TestCase):
+
+	def test_camel_case(self):
+		self.assertEqual('helloWorld', camel.camel_case('Hello World'))
+
+	def test_camel_case1(self):
+		self.assertEqual('', camel.camel_case(''))
+
+	def test_camel_case_spaces(self):
+		self.assertEqual('helloWorld', camel.camel_case('	Hello   World   '))
+
+if __name__ == '__main__':
+    unittest.main()
