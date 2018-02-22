d1429404a7c1676ceb6a79d9c1654da639355f89
JL <lollypopsandrainbows@gmail.com>
2018-02-22 17:55:07 -0600

    Language and search Function

--- WikipediaAPI.py
+++ WikipediaAPI.py
@@ -3,3 +3,8 @@
 
 import wikipedia #imports wiki function
 print wikipedia.summary("Wikipedia") #prints wiki's tagline/headline info
+
+
+def wiki_main():
+	wikipedia.set_lang("en") #sets language to english
+	wikipedia.search(search_string) #TODO Get user input search string
