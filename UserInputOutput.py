aed0dfcb1c06d3a97869a310bedea38fcc742f62
JL <lollypopsandrainbows@gmail.com>
2018-02-22 18:22:01 -0600

    Added city/country validation. Searches to see if string contains a space

--- UserInputOutput.py
+++ UserInputOutput.py
@@ -3,4 +3,13 @@ import youtubeAPI
 import weatherapi
 
 def user_input():
-	search_string = input("Please enter a location (city, country)")
+	input = True
+	while (input == True):
+		search_string = input("Please enter a location (city, country)")
+		if " " not in search_string: #simple validation. Checks for space
+		input = True
+		else:
+		input = False
+		
+def results_print():
+	
