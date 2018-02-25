b1fa074394d9d35b96e03f0a1f7b784390650a9f
Julie <jhchiu1@gmail.com>
2018-02-25 17:47:30 -0600

    Add search methods

--- WikipediaAPI.py
+++ WikipediaAPI.py
@@ -4,7 +4,47 @@
 import wikipedia #imports wiki function
 print wikipedia.summary("Wikipedia") #prints wiki's tagline/headline info
 
-
 def wiki_main():
 	wikipedia.set_lang("en") #sets language to english
 	wikipedia.search(search_string) #TODO Get user input search string
+
+# Returns number of search results
+def getNumberOfResults(query):
+    results = wikipedia.search(query,suggestion=True)
+    return (len(results[0]))
+
+# Returns search suggestions if user incorrectly spelled a word
+def getSuggestions(query):
+    suggestions = wikipedia.suggest(query)
+    return suggestions
+
+# Returns list of image links from search
+def getImages(query):
+    page = wikipedia.page(query)
+    images = page.images
+    return images
+
+# Returns full list of possible results
+def getSearchResults(query):
+    suggestions = wikipedia.search(query)
+    return suggestions[:4]
+
+# Returns article of title
+def getTitle(query):
+    page = wikipedia.page(query)
+    return page.title
+
+# Returns article summary
+def getSummary(query):
+    summary = wikipedia.summary(query, sentences=1)
+    return summary
+
+# Returns the page URL
+def getURL(query):
+    page = wikipedia.page(query)
+    url = page.url
+    return url
+
+def getPage(query):
+    page = wikipedia.page(query)
+    return page
