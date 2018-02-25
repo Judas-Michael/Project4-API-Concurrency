20396cc05623d357e9cbe42c79d8a331324a245a
Julie <jhchiu1@gmail.com>
2018-02-25 17:40:15 -0600

    Modify search method, WIP: formated url

--- weatherapi.py
+++ weatherapi.py
@@ -1,9 +1,8 @@
 # Weather API
 # Code sample from https://www.wunderground.com/weather/api/d/docs?d=resources/code-samples&MR=1
-
 import requests
 import urllib.request
-import ujson as json
+import json
 
 # Gets the key from the file
 # Reads file and returns key
@@ -14,24 +13,24 @@ def getKey():
     return key
 
 ###### WORK IN PROGRESS
-class WeatherAPI:
-	def __init__(self,search):
-		self.search = search
-		self.baseURL = ""
+class WeatherAPISearch:
+	def __init__(self):
+		self.key = getKey()
 
-# URL where API is called
-call = urllib.request.urlopen('http://api.wunderground.com/api/66bc345f90882b9f/geolookup/conditions/q/CA/San_Jose.json')
-json_string = call.read()
-parsed_json = json.loads(json_string) 
+	defnewscast(self)
+		list = []
 
-# Get key data pairs
-location = parsed_json['location']['city'] #takes parts of library and assigns to value
-temp_f = parsed_json['current_observation']['temp_f']
-Logger.info(This combines information to make it easier to read, *args, **kwargs)¶
+		# URL where API is called
+		call = urllib.request.urlopen('http://api.wunderground.com/api/geolookup/conditions/q/CA/San_Jose.json')
+		json_string = call.read()
+		parsed_json = json.loads(json_string)
 
+		# Get key data pairs
+		location = parsed_json['location']['city']
+		temp_f = parsed_json['current_observation']['temp_f']
 
-# Print temperature based on city location
-print ("Current temperature in %s is: %s" % (location, temp_f))
-call.close()
+		# Print temperature based on city location
+		print ("Current temperature in %s is: %s" % (location, temp_f))
 
+		call.close()
 
